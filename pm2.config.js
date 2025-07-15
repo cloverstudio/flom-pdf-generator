54f76e9594623ae4973382590a8ab3ee83ab283e
petarb1988 <amper.sand@yahoo.com>
2025-07-15 18:01:34 +0200

    update

--- pm2.config.js
+++ pm2.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "flom-pdf-generator",
+      name: "pdf-generator",
       script: "./src/app.js",
       watch: false,
       interpreter: "/home/pm2user/.nvm/versions/node/v22.17.0/bin/node",
